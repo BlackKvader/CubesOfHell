30a5257c06eec9c7c4163bab5b177ee67a9df96f
BlackKvader <black.kvader@gmail.com>
2015-12-08 14:55:17 +0100

    add also Tcube.cpp and hpp

---
+++ Tcube.cpp
@@ -0,0 +1,14 @@
+/*
+  most basic class for structure
+  contains pointers to neighbours (and derfore no boundary conditions)
+  also option to endure structure with second derivative
+  or conect like single spring
+  also check for maximum angles/distance
+  and of course makeStep() that checks if there is colision.
+*/
+
+// this part go to Tcube.hpp later
+class Tcube{
+public:
+  void makeStep(); //makes single step
+}
