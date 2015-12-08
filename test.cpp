bb510fd9b90394569e1d8b96cced90155a6d0032
BlackKvader <black.kvader@gmail.com>
2015-12-08 14:54:07 +0100

    change test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,26 @@
-#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
+class Tskuska{
+public:
+  draw();
+}
+
+Tskuska::draw(){
+	cout << "ok, draw called";
+}
+
 int main(){
+	Tskuska skuska; // creating skuska
+	
 	cout << "CubesOfHell";
 	cout << "OK, let's do something!";
   if (3>1) {
     cout << "ok, we are geting somewhere";
     cout << "It just not wery good, just for coments";
+    cout << "why is syntax highlighting not working?";
   }
+  
+  skuska.draw();  //test call
 }
