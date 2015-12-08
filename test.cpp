7b794507cefbfb95d49e0cee40a951789bd31451
BlackKvader <black.kvader@gmail.com>
2015-12-08 12:48:23 +0100

    and Tcube.cpp notes

--- test.cpp
+++ test.cpp
@@ -5,4 +5,8 @@ using namespace std;
 int main(){
 	cout << "CubesOfHell";
 	cout << "OK, let's do something!";
+  if (3>1) {
+    cout << "ok, we are geting somewhere";
+    cout << "It just not wery good, just for coments";
+  }
 }
