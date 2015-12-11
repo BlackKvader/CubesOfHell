1a8411056830fe88ab79a19fa4b6e4552b2d6ae9
BlackKvader <black.kvader@gmail.com>
2015-12-11 19:34:11 +0100

    almost finished

--- Tvector.hpp
+++ Tvector.hpp
@@ -2,8 +2,7 @@
 // and also TintVec which is integer vector
 // some basic operations with vectors
 
-// dodat pretazenie operatorov
-// finish setL and null functions
+// Also additional functions outside the class
 
 #include <cmath>
 
@@ -18,7 +17,12 @@ public:
 	double l(); // length of vector
 	void calcL(); // set length vector
 	double scalar(Tvector &v2)
-}
+
+	// operator overloading
+	const Tvector& operator-(const Tvector& v2);
+	const Tvector& operator+(const Tvector& v2);
+	void operator= (const Tvector& v2);
+};
 
 void Tvector::add(Tvector &v1){
 	x+=v1.x;
@@ -49,19 +53,76 @@ double Tvector::scalar(Tvector &v2){
 	return x*v2.x+y*v2.y+z*v2.z;
 }
 
-/******  TintVec class   *********/
+const Tvector& Tobj::operator-(const Tvector& v2){
+	obj result = *this;
+	result.x=result.x-v2.x;
+	result.y=result.y-v2.y;
+	result.z=result.z-v2.z;
+	return result;
+}
 
+const Tvector& Tobj::operator+(const Tvector& v2){
+	obj result = *this;
+	result.x=result.x+v2.x;
+	result.y=result.y+v2.y;
+	result.z=result.z+v2.z;
+	return result;
+}
+
+void Tobj::operator= (const Tvector& v2){
+	x=v2.x;
+	y=v2.y;
+	z=v2.z;
+}
+
+/********  out of the class overloaded operators  ******/
+const Tobj operator* (double k2, const Tvector &v2){
+	Tvector result;
+	result.x=v2.x*k2;
+	result.y=v2.y*k2;
+	result.z=v2.z*k2;
+	return result;
+}
+
+const Tobj operator* (const Tvector &v2, double k2){
+	Tvector result;
+	result.x=v2.x*k2;
+	result.y=v2.y*k2;
+	result.z=v2.z*k2;
+	return result;
+}
+
+const Tobj operator/ (const Tvector &v2, double k2){
+	Tvector result;
+	result.x=v2.x/k2;
+	result.y=v2.y/k2;
+	result.z=v2.z/k2;
+	return result;
+}
+
+/******* length function   ********/
+
+double vecLen(Tvector &v2){
+	return (v2.x*v2.x+v2.y*v2.y+v2.z*v2.z)
+}
+
+/******  TintVec class   *********/
 class TintVec{
 public:
 	int x,y,z;
 	double l; // can be used if you call l() much times
 	
-	void add(Tvector &v1);
+	void add(TintVec &v1);
 	double l(); // length of vector
 	void calcL(); // set length vector
-}
 
-void TintVec::add(Tvector &v1){
+	// operator overloading
+	const TintVec& operator-(const TintVec& v2);
+	const TintVec& operator+(const TintVec& v2);
+	void operator= (const TintVec& v2);
+};
+
+void TintVec::add(TintVec &v1){
 	x+=v1.x;
 	y+=v1.y;
 	z+=v1.z;
@@ -75,3 +136,24 @@ void TintVec::calcL(){
 	l=x*x+y*y+z*z;
 }
 
+const TintVec& Tobj::operator-(const TintVec& v2){
+	obj result = *this;
+	result.x=result.x-v2.x;
+	result.y=result.y-v2.y;
+	result.z=result.z-v2.z;
+	return result;
+}
+
+const TintVec& Tobj::operator+(const TintVec& v2){
+	obj result = *this;
+	result.x=result.x+v2.x;
+	result.y=result.y+v2.y;
+	result.z=result.z+v2.z;
+	return result;
+}
+
+void Tobj::operator= (const TintVec& v2){
+	x=v2.x;
+	y=v2.y;
+	z=v2.z;
+}
