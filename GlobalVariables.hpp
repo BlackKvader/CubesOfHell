9da66ea06c06e8ed4528506bc34e4d9bfeabc151
BlackKvader <black.kvader@gmail.com>
2015-12-11 19:27:47 +0100

    Create GlobalVariables.hpp

---
+++ GlobalVariables.hpp
@@ -0,0 +1,12 @@
+// header for global variables
+
+#ifndef _GlobalVariables_hpp
+#define _GlobalVariables_hpp
+
+extern const double cube_side; // side of Tcube. It is same for all world
+extern const double sqrt2_side; // sqrt(2)*cube_side
+extern const double sqrt3_side; // sqrt(3)*cube_side
+
+extern void initGlobalVariables(); // global init of variables. Call just onece
+
+#endif
