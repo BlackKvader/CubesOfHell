4c7609d8ab4114b608f8969b2e3304908fdc72c2
BlackKvader <black.kvader@gmail.com>
2015-12-11 19:29:13 +0100

    Update Tcube.hpp

--- Tcube.hpp
+++ Tcube.hpp
@@ -1,19 +1,23 @@
 /*
   Just a network used for other strutures
+  list of content in Pobj (pointer to objects)
 */
 
-#ifndef _Tcube_hpp
+#ifndef _Tcube_hpp // guardian of treasure :)
 #define _Tcube_hpp
 
 #include "Tvector.hpp"
 #include "Tobj.hpp"
 
+#define max_inside 3 	// maximum number of Tobj of same structure inside one Tcube.
+  						// Tobj inside Tcube interact therefore keep low.
+
 // this part go to Tcube.hpp later
 class Tcube{
 public:
-  Tvector pos;
-  bool bool_ocupied; // if is something inside
-  Tobj* PObj; // pointer tovobject inside
-}
+  Tvector pos; // position in the world
+  char n_inside; // number of Tobj inside
+  Tobj* Pobj[max_inside]; // pointer tovobject inside
+};
 
 #endif
